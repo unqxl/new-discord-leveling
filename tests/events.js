04e8693fe4a6bae498aec516c6b2b1b27c9b5100
bad-boy-discord <46648824+bad-boy-discord@users.noreply.github.com>
2021-05-10 18:28:22 +0300

    Update events.js

--- tests/events.js
+++ tests/events.js
@@ -5,5 +5,5 @@ const leveling = new Leveling({
 });
 
 (async() => {
-    console.log(await leveling.get('545956523571150858', '826084972279365652'));
+    console.log(await leveling.get('545956523571150858', '826084972279365652')); // 'get' Method.
 })();
