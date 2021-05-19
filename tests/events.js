b2ebbb81d78f5ad55b2780ce8635f4c4f74d8306
bad-boy-discord <46648824+bad-boy-discord@users.noreply.github.com>
2021-05-19 23:26:20 +0300

    Delete tests directory

--- tests/events.js
+++
@@ -1,9 +0,0 @@
-const Leveling = require('../src/js/index');
-const leveling = new Leveling({
-    type: 'json',
-    jsonPath: './tests/db.json'
-});
-
-(async() => {
-    console.log(await leveling.get('545956523571150858', '826084972279365652')); // This is 'get' Method.
-})();
